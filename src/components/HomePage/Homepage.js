9a30ebe71fe4499c82c26cd5073505d12c66f645
Michał Micherda <mmicherdakr@gmail.com>
2021-08-14 10:36:49 +0200

    Add basic build + homepage

---
+++ src/components/HomePage/Homepage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Aos from 'aos';
+import homepage_bg_video from '../../media/video.mp4';
+import './Homepage.scss';
+
+const Homepage = () => {
+    Aos.init({duration: 1500});
+    return (
+        <section className='homepage_section' id='home'>
+            <video
+                autoPlay
+                loop
+                muted
+                className='homepage_bg_video'
+                >
+                <source src={homepage_bg_video} type='video/mp4' />
+            </video>
+            <h1 className='homepage_title' data-aos='fade-right'>Hello!<br/>I'm Mike.</h1>
+            <div className='homepage_subtitle' data-aos='fade-left'>Let's create <br/>your website together!</div>
+            <div className='homepage_black_square'></div>
+            <div className='scroll_icon'>Start scrolling</div>
+            <div className='mouse_object'></div>
+        </section>
+    );
+};
+
+export default Homepage;
